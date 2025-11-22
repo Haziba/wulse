0947b4fd35064867d749038c8b0babfa1791eb51
Harry Boyes <harry.boyes@gmail.com>
2025-11-22 20:05:38 +0100

    fix: Add back in subdomain on welcome email

--- app/mailers/staff_mailer.rb
+++ app/mailers/staff_mailer.rb
@@ -3,7 +3,7 @@ class StaffMailer < ApplicationMailer
     @staff = staff
     @institution = staff.institution
     @password_reset = password_reset
-    @set_password_url = edit_password_reset_url(@password_reset.token)
+    @set_password_url = edit_password_reset_url(@password_reset.token, subdomain: @institution.subdomain)
 
     mail to: @staff.email,
          subject: "Welcome to the #{@institution.name} Digital Library!"
