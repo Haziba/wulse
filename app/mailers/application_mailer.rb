82c1d0985c7b3723f05dfec585b84ee12ad4f755
Harry Boyes <harry.boyes@gmail.com>
2025-11-22 18:26:17 +0100

    feat: Set up action mailer for prod

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: -> {
+    name = Current.try(:institution)&.name || "Wulse"
+    "#{name} Digital Library <noreply@wulse.org>"
+  }
   layout "mailer"
+
+  def default_url_options
+    {
+      host: Current.host || ENV.fetch("APP_HOST", "wulse.org"),
+      protocol: "https"
+    }
+  end
 end
