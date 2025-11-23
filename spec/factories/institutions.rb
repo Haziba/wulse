f89608ce3ed04012761ef46f27057d548a855062
Harry Boyes <harry.boyes@gmail.com>
2025-11-23 23:17:22 +0100

    chore: Make storage totals into bigint

--- spec/factories/institutions.rb
+++ spec/factories/institutions.rb
@@ -5,7 +5,7 @@
 #  id              :uuid             not null, primary key
 #  branding_colour :string           not null
 #  name            :string           not null
-#  storage_total   :integer          default(0), not null
+#  storage_total   :bigint           default(0), not null
 #  storage_used    :bigint           default(0), not null
 #  subdomain       :string           not null
 #  created_at      :datetime         not null
