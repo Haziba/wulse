ebd77fdb57d6016ad8d2d2ee18c8ad1751c62d70
Harry Boyes <harry.boyes@gmail.com>
2025-11-23 19:35:42 +0100

    fix: Handle no filters on library index

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -7,7 +7,7 @@ class LibraryController < ApplicationController
     filtered_scope = Library::DocumentFilter.call(params)
     @pagy, @documents = pagy(filtered_scope, limit: 10)
     @query = params[:q]
-    @filters = Library::FilterCounts.new(filtered_scope, selected_filters: decode_filter_params).call
+    @filters = Library::FilterCounts.new(filtered_scope, selected_filters: filter_params).call
     @filtered_count = filtered_scope.count
     @total_count = Document.count
 
@@ -29,5 +29,11 @@ class LibraryController < ApplicationController
     Library::FilterParams.decode(params[:f]).each do |key, values|
       params[key] = values
     end
+  end
+
+  def filter_params
+    return {} if params[:f].blank?
+
+    Library::FilterParams.decode(params[:f]) || {}
   end
 end
