08f3bdf6ad83816313dc742a3af9f1c2ce5fea2c
Harry Boyes <harry.boyes@gmail.com>
2025-11-24 19:18:34 +0100

    chore: Satisfy bullet some more

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
       render :landing, layout: false
     else
       @recent_documents = Current.institution.documents
-                                  .includes(:preview_image_attachment, :file_attachment)
+                                  .includes(preview_image_attachment: :blob, file_attachment: :blob)
                                   .order(created_at: :desc)
                                   .limit(3)
     end
